5d21a6050702aa3cafc4da1fae1e7053010b98b9
Vinsm-L <vinsm@foxmail.com>
2016-11-17 21:34:15 +0800

    Update tree.hpp

--- tree.hpp
+++ tree.hpp
@@ -12,7 +12,9 @@ public:
 		string name;
 		bool gender;  // 0 represents male while 1 represents female
 		bool alive;  // 0 represents dead while 1 represents alive
-		Tnode* left, right, forward; // forward points to its parent node
+		Tnode* left;
+		Tnode* right;
+		Tnode* forward; // forward points to its parent node
 		Tnode(string n = "-1", bool g = 0, bool a = 0, Tnode* l = NULL, Tnode* r = NULL, Tnode* f = NULL) {
 			name = n;
 			gender = g;
@@ -27,8 +29,12 @@ public:
 		string name, gender, alive;               //  some string should be converted to bool in some way
 		string wifeName, wifeGender, wifeAlive;
 		int kidsNumber, siblingNumber;
-		string* kidsName, kidsGender, kidsAlive;
-		string* siblingName, siblingGender, siblingAlive;
+		string* kidsName;
+		string* kidsGender;
+		string* kidsAlive;
+		string* siblingName;
+		string* siblingGender;
+		string* siblingAlive;
 	};
 
 	tree();  //  default constructer
